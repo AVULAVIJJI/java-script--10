d32ceec7999f7b40686279966e607ca302778289
AVULAVIJJI <20kb1a1205@nbkrist.org>
2024-06-11 11:39:29 +0530

    tst

--- .vscode/conditional statements/3digits.js
+++
@@ -1,7 +0,0 @@
-let num = 34
-let numstr = num.toString()
-if( numstr.length === 3){
-    console.log(num + "is a 3 DIgit number")
-}else{
-    console.log(num + "is not a 3 digit number")
-}
--- .vscode/conditional statements/divisibleby3.js
+++
@@ -1,6 +0,0 @@
-let testnumber = 24
-if (testnumber % 3 === 0){
-    console.log(testnumber + "is the mutiple of 3")
-}else{
-    console.log(testnumber+"is the not mutiple of 3")
-}
