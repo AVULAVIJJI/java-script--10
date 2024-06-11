a74cd46f17ac646ac44de5846537656053c7b4d6
AVULAVIJJI <20kb1a1205@nbkrist.org>
2024-06-11 12:16:45 +0530

    leastnumber

--- conditional statements/leastnum.js
+++ conditional statements/leastnum.js
@@ -1,6 +1,14 @@
-let number = 371
-if(number %2===0){
-    console.log(number+"its is even number")
-}else{
-    console.log(number+"its odd numer")
+function findMinimumNumber(num1, num2) {
+    if (num1 < num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 }
+
+// Example usage:
+let number1 = 10;
+let number2 = 20;
+
+let minimumNumber = findMinimumNumber(number1, number2);
+console.log("The least number is: " + minimumNumber);
